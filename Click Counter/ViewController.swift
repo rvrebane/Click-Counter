8cbbe9035a8a03df824c28583caf718dd38de708
Rein Saetalu <reinukas1@gmail.com>
2021-08-14 19:03:58 +0300

    Initial Commit

---
+++ Click Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Rein Saetalu on 14.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
